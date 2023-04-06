ec1c7aee206296078d355016bb6366f5a8bcc42e
trananh195 <trananhtnu195@gmail.com>
2023-04-06 10:40:40 +0700

    trần anh vừa tạo fb1

---
+++ jsbuoi3/dssanpham.js
@@ -0,0 +1,43 @@
+//code js
+// chứa thông tin của danh sách/ mảng thông tin các sản phẩm(tensp, mota, anhsp)
+let dssanpham = [];
+let sp1 = {
+    tensp: "Natural 1",
+    mota: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, odit. Asperiores, sed ea consequatur eveniet suscipit eum rerum sunt laborum necessitatibus, sequi consequuntur nesciunt esse",
+    anhsp: "natural/10.jpg"
+};
+dssanpham.push(sp1);
+dssanpham=[
+    {
+    tensp: "Natural 1",
+    mota: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, odit. Asperiores, sed ea consequatur eveniet suscipit eum rerum sunt laborum necessitatibus, sequi consequuntur nesciunt esse",
+    anhsp: "natrural/10.jpg"
+
+    },
+    {
+    tensp: "Natural 2",
+    mota: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, odit. Asperiores, sed ea consequatur eveniet suscipit eum rerum sunt laborum necessitatibus, sequi consequuntur nesciunt esse",
+    anhsp: "natrural/11.jpg"
+    },
+    {
+        tensp: "Natural 3",
+    mota: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, odit. Asperiores, sed ea consequatur eveniet suscipit eum rerum sunt laborum necessitatibus, sequi consequuntur nesciunt esse",
+    anhsp: "natrural/13.jpg"
+    },
+    {
+    tensp: "Natural 4",
+    mota: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, odit. Asperiores, sed ea consequatur eveniet suscipit eum rerum sunt laborum necessitatibus, sequi consequuntur nesciunt esse",
+    anhsp: "natrural/29.jpg"
+    },
+    {
+    tensp: "Natural 5",
+    mota: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, odit. Asperiores, sed ea consequatur eveniet suscipit eum rerum sunt laborum necessitatibus, sequi consequuntur nesciunt esse",
+    anhsp: "natrural/49.jpg"
+    },
+    {
+    tensp: "Natural 6",
+    mota: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, odit. Asperiores, sed ea consequatur eveniet suscipit eum rerum sunt laborum necessitatibus, sequi consequuntur nesciunt esse",
+    anhsp: "natrural/49.jpg"
+    }
+
+];
